77f5f9690e1153accea52e066282d37a0638d77d
GwonHeeJun <gwonhj9751@naver.com>
2020-10-25 19:21:44 +0900

    modify : init backend

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+import Koa from 'koa';
+import Router from 'koa-router';
+import cors from '@koa/cors';
+
+const app = new Koa();
+const router = new Router();
+
+import api from './api';
+
+import bodyParser from 'koa-bodyparser';
+
+app.use(cors());
+
+const port = process.env.PORT || 4000;
+
+app.use(bodyParser());
+router.use('/api', api.routes());
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(port, () => {
+    console.log(`HSDL Backend Server Started.. with port ${port}`);
+});
